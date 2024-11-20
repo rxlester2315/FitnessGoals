8915934bf8b5ebc1273907582b70c10f8316f852
Rex <rexlesterbastaoang0811@gmail.com>
2024-11-20 22:29:48 +0800

    grownson

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    
+
+        public function authregister(){
+
+        return view('Auth.register');
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+
+
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/login',[LoginController::class,'authlog']);
+Route::get('/register',[RegisterController::class,'authregister']);
